fee5716628abcb6553a5348b8a9e88e110cb062d
miguelboing <miguel-boing@hotmail.com>
2025-05-21 12:45:48 +0100

    dataset: Workaround to avoid future depracated functionalities #6

--- dataset/dataset.py
+++ dataset/dataset.py
@@ -2,7 +2,7 @@ import pandas as pd
 
 class Dataset:
     def __init__(self, filename, columns):
-        self.df = pd.DataFrame(columns=columns)
+        self.df = pd.DataFrame(columns=columns, dtype="object")
         self.filename = filename
 
     # Export data into a dictionary
@@ -11,7 +11,11 @@ class Dataset:
 
     # Add new samples
     def add_row(self, row):
-        self.df = pd.concat([self.df, row], ignore_index = True)
+        row = row[self.columns]
+        if (self.df.empty):
+            self.df = row.copy()
+        else:
+            self.df = pd.concat([self.df, row], ignore_index = True, copy=False)
 
     #Use this method to import the csv to a dataframe
     @classmethod
@@ -23,15 +27,15 @@ class Dataset:
         self.df.to_csv(self.filename, index=False)
 
 class DecodeDataset(Dataset):
+    columns = ["wsjtx_id", "new_decode", "millis_since_midnight", "time",
+                    "snr", "delta_t", "delta_f", "mode", "message", "low_confidence",
+                    "off_air"]
     def __init__(self):
-        Dataset.__init__(self, './dataset/output/' + 'decodedataset.csv', ["wsjtx_id", "new_decode", "millis_since_midnight", "time",
-                                "snr", "delta_t", "delta_f", "mode", "message", "low_confidence",
-                                "off_air"])
+
+        Dataset.__init__(self, './dataset/output/' + 'decodedataset.csv', self.columns)
 
     def add_new_sample(self, sample_dict):
-        df_temp = pd.DataFrame.from_dict({'name':sample_dict.keys(), 'value':sample_dict.values()})
+
+        df_temp = pd.DataFrame([sample_dict], columns=self.columns)
 
         self.add_row(df_temp)
-        self.df.to_csv(self.filename, index=False)
-        import sys
-        sys.exit(0)
