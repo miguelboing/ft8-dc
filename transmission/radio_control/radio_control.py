a9165504fdb48fecbfd4961de383424c642c0b10
miguelboing <miguel-boing@hotmail.com>
2025-06-09 16:58:30 +0100

    transmission/radio_control: Adding functions to handle TX power #16

--- transmission/radio_control/radio_control.py
+++ transmission/radio_control/radio_control.py
@@ -1,15 +1,19 @@
 import subprocess
 
 class RadioControl:
-    def __init__(self, m='2', port='localhost:4532'):
+    def __init__(self, m='2', port='localhost:4532', max_power_W=100):
         self.m = m
         self.port= port
+        self.max_power_W = max_power_W
 
     def get_if_frequency(self):
         return subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'f'], capture_output=True, text=True).stdout.strip()
 
     def get_mode(self):
         return subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'm'], capture_output=True, text=True).stdout.strip()
+
+    def get_tx_power(self):
+        return round(self.max_power_W * float(subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'l', 'RFPOWER'], capture_output=True, text=True).stdout.strip()))
 
     def set_if_frequency(self, frequency=14074000):
         subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'F', str(frequency)])
@@ -32,6 +36,25 @@ class RadioControl:
             return -1
         return 0
 
+    def set_tx_power(self, power_W):
+        if (power_W > self.max_power_W):
+            print(f"Failed to change the TX power, maximum power is {self.max_power_W} but request to set power to {power_W}")
+            return -1
+
+        power = round(power_W/self.max_power_W, 1)
+        subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'L', 'RFPOWER', str(power)])
+
+        cur_power = self.get_tx_power()
+        if (cur_power != power_W):
+            print(f"Failed to change the TX power, tried to change to {power_W} ({power}), but current power is {cur_power} ({round(float(cur_power)/self.max_power_W,1)})")
+
+            return -1
+
+        return 0
+
+
+
+
 def main():
     radio = RadioControl()
     print("Hello World!")
@@ -42,11 +65,15 @@ def main():
 #
 #    print(radio.get_if_frequency())
 
-    print(radio.get_mode())
+    print(radio.get_tx_power())
 
-    print(radio.set_mode('USB', 2700))
+    print(radio.set_tx_power(10))
 
-    print(radio.get_mode())
+    print(radio.get_tx_power())
+
+#    print(radio.set_mode('USB', 2700))
+#
+#    print(radio.get_mode())
 
     return 0
 
