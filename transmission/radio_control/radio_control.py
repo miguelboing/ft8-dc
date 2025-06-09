181cc2f5f866f304edc0963199dd1727a2d2cfb6
miguelboing <miguel-boing@hotmail.com>
2025-06-09 14:32:35 +0100

    transmission: radio_control: Adding set/get mode rigctl command support #16

--- transmission/radio_control/radio_control.py
+++ transmission/radio_control/radio_control.py
@@ -6,17 +6,30 @@ class RadioControl:
         self.port= port
 
     def get_if_frequency(self):
-        return subprocess.run(['rigctl', '-m', '2', '-r', 'localhost:4532', 'f'], capture_output=True, text=True).stdout.strip()
+        return subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'f'], capture_output=True, text=True).stdout.strip()
+
+    def get_mode(self):
+        return subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'm'], capture_output=True, text=True).stdout.strip()
 
     def set_if_frequency(self, frequency=14074000):
-        subprocess.run(['rigctl', '-m', '2', '-r', 'localhost:4532', 'F', str(frequency)])
+        subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'F', str(frequency)])
 
         cur_freq = self.get_if_frequency()
 
         if (cur_freq != str(frequency)):
-            print("Failed to set the frequency currently set to: {result}")
+            print("Failed to set the frequency. Currently set to: {result}")
             return -1
+        return 0
 
+    def set_mode(self, mode='LSB', passband=-1):
+        # passband is in Hz. passband = -1 for no change, passband = 0 for radio backend default
+        subprocess.run(['rigctl', '-m', self.m, '-r', self.port, 'M', mode + ' ' + str(passband)])
+
+        cur_mode = self.get_mode()
+
+        if (cur_mode != mode):
+            print("Failed to mode. Currently set to: {cur_mode}")
+            return -1
         return 0
 
 def main():
