758209528793e030f242680df9cbd459efbc4ee1
miguelboing <miguel-boing@hotmail.com>
2025-07-11 23:16:55 +0100

    dataset/psk_reporter: Saving parameters #19, #8

--- dataset/psk_reporter/psk_reporter_httpclient.py
+++ dataset/psk_reporter/psk_reporter_httpclient.py
@@ -1,10 +1,11 @@
 import requests
 import pandas as pd
 from io import StringIO
-import listernerstationsclusters as lsc
+import listernerstationclusters as lsc
 
-class PSK_Reporter:
+class HTTP_Client(lsc.ListernerStationClusters):
     url = "https://retrieve.pskreporter.info/query"
+
     def __init__(self, callsign):
         self.senderCallsign = callsign,
 
@@ -17,27 +18,36 @@ class PSK_Reporter:
         # Sending a request to PSK Reporter
         r = requests.get(url, params=params)
 
+        report = {}
+
         # If the XML is HTML-escaped (e.g., &lt; and &gt;), decode it first
         xml_string = r.text.replace("&lt;", "<").replace("&gt;", ">")
 
         # Reports that contains the given callsign.
         df_reception_report = pd.read_xml(StringIO(xml_string), xpath=".//receptionReport")
+        report['reception_reports'] = df_reception_report
 
         # Callsigns that were recently reported as active.
         df_active_cs = pd.read_xml(StringIO(xml_string), xpath=".//activeCallsign")
+        report['active_cs'] = len(df_2)
 
         # This are the stations that are currently active.
         df_active_receiver = pd.read_xml(StringIO(xml_string), xpath=".//activeReceiver")
-        clusters = lsc.ListenerStationClusters(df_active_receiver)
-        clusters.save_to_pklgz(name)
+        lsc.ListernerStationClusters.__init__(self, df_reception_report, 11)
+        report['receptions'] = self.cluster_params
 
         # Contains the senderCallsign and the most recent unix epoch of when a transmission from senderCallsign was reported.
-        df_sender_search = pd.read_xml(StringIO(xml_string), xpath=".//senderSearch")
+        #df_sender_search = pd.read_xml(StringIO(xml_string), xpath=".//senderSearch")
 
         # Unique identifier for the PSK Reporter request, not really useful for the dataset.
-        df_last_sequence_number = pd.read_xml(StringIO(xml_string), xpath=".//lastSequenceNumber")
+        #df_last_sequence_number = pd.read_xml(StringIO(xml_string), xpath=".//lastSequenceNumber")
 
         # Unix epoch of the last report contained in this response.
         df_max_flow_start_seconds = pd.read_xml(StringIO(xml_string), xpath=".//maxFlowStartSeconds")
+        report['last_report_time'] = df_max_flow_start_seconds['value'][0]
+
+        print(report)
+
+        return report
 
 
