c4a2a8a53de50863db82d8fbcd6b4caea2e75c71
miguelboing <miguel-boing@hotmail.com>
2025-07-13 18:06:00 +0100

    ham-amdc/ Moving application behaviour from main #24

--- ham-amdc/__main__.py
+++ ham-amdc/__main__.py
@@ -2,45 +2,17 @@ import os
 import sys
 import pathlib
 
+from ham_amdc import HamAMDC
+
 sys.path.append(str(pathlib.Path(os.path.realpath(__file__)).parents[1]))
 
-from dataset.dataset import DecodeDataset
-from wsjtx_server.wsjtx_server import WSJTXUDPServer
-
-import signal
-
 def main():
+    HamAMDC()
 
-    # Program starts after collecting status info
-    _, pkt = server.receive_pkt({'StatusPacket'})
-    output.set_status_info(pkt)
+    return 0
 
-    print("Starting to collect samples...")
-
-    while(True):
-        # Hook that reads only the wanted packets
-        pkt_type, pkt = server.receive_pkt({'DecodePacket', 'StatusPacket'})
-        if (pkt_type == 'StatusPacket'):
-            # Update internal values
-            output.set_status_info(pkt)
-
-        elif (pkt_type == 'DecodePacket'):
-            output.add_new_sample(pkt)
-
-
-def exit_program(sig, frame):
-    print("\nSafely exiting the application...")
-    output.save_csv()
-    #TODO: Close port properly
-
-    sys.exit(0)
 
 if __name__ == '__main__':
-
-    output = DecodeDataset()
-    server = WSJTXUDPServer()
-
-    signal.signal(signal.SIGINT, exit_program)
 
     main()
 
