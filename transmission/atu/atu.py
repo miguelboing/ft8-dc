2b239586b7827135cc0aa5b4d7b897edf031de4d
miguelboing <miguelboing13@gmail.com>
2025-09-12 22:14:10 +0100

    transmission/atu: Adding support for flex6xxx swr reading #36

--- transmission/atu/atu.py
+++ transmission/atu/atu.py
@@ -4,16 +4,81 @@ import time
 def flex6xxx_atu():
     UDP_IP = "" # INADDR_ANY
     UDP_PORT = 4992
+    SWR_R_UDP_PORT = 4200
     TCP_PORT = 4992
     BUFFER_SIZE = 16384
 
     BASE_CMD_NUM = 1000000  # Start at 1 million
     MAX_CMD_NUM = 1500000 # Ends at 1.5 million and then loops back
 
+    swr = -1
+
+    def flush_tcp_buffer():
+        try:
+            flex6xxx_atu.tcp_socket.settimeout(0.1)  # Short timeout
+            while True:
+                flex6xxx_atu.tcp_socket.recv(BUFFER_SIZE)  # Drain buffer
+        except socket.timeout:
+            pass  # Buffer is now empty
+        finally:
+            flex6xxx_atu.tcp_socket.settimeout(10)  # Restore normal timeout
+
+    def send_tcp_command(command):
+
+        flush_tcp_buffer()
+
+        message = f"C{flex6xxx_atu.cmd_counter}|{command}\n"
+        flex6xxx_atu.tcp_socket.send(message.encode("cp1252"))
+
+        time.sleep(1)
+        data_tcp = flex6xxx_atu.tcp_socket.recv(BUFFER_SIZE)
+        print(data_tcp)
+
+        expected_response = f"R{flex6xxx_atu.cmd_counter}|0|"
+
+        flex6xxx_atu.cmd_counter += 1
+        if (flex6xxx_atu.cmd_counter > MAX_CMD_NUM):
+            flex6xxx_atu.cmd_counter = BASE_CMD_NUM
+
+        if not data_tcp or (data_tcp.splitlines()[0] != expected_response.encode()):
+                raise ValueError(f"atu: Failed send command: {message}!")
+
+
+    def swr_enable_socket():
+        # UDP Port to the SWR
+        flex6xxx_atu.udp_swr_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+        flex6xxx_atu.udp_swr_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        flex6xxx_atu.udp_swr_socket.settimeout(10)
+        flex6xxx_atu.udp_swr_socket.bind(("", SWR_R_UDP_PORT))  # Add this line
+
+        return
+
+    def swr_disable_socket():
+        flex6xxx_atu.udp_swr_socket.close()
+
+        return
+
+    def swr_read_value():
+
+        send_tcp_command(f"transmit tune on")
+        swr_enable_socket()
+
+        data, _ = flex6xxx_atu.udp_swr_socket.recvfrom(1024)
+
+        if (data.hex()[-6:-4] == "0a"): # Checking if the received parameter is actually the SWR
+            swr = int.from_bytes(data[-2:], byteorder='big')/128
+        else:
+            swr = -1
+
+        send_tcp_command(f"transmit tune off")
+        swr_disable_socket()
+
+        return swr
+
     if not hasattr(flex6xxx_atu, "cmd_counter"):
         flex6xxx_atu.cmd_counter = BASE_CMD_NUM
 
-    # This checks if the discovery step is already done
+    # This checks if the discovery step has been done
     if not hasattr(flex6xxx_atu, "tcp_addr"):
         print("atu: Starting discovery...")
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -34,6 +99,7 @@ def flex6xxx_atu():
     if getattr(flex6xxx_atu, "tcp_socket", None) is None:
         print("Establishing TCP Connection...")
 
+        # TCP Port to send API commands
         flex6xxx_atu.tcp_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         flex6xxx_atu.tcp_socket.settimeout(10)
 
@@ -53,36 +119,21 @@ def flex6xxx_atu():
             flex6xxx_atu.tcp_socket = None
             raise RuntimeError(f"atu: Failed to connect to radio: {e}")
 
+        send_tcp_command(f"transmit set tunepower=10")
+        send_tcp_command(f"sub meter 10")
+        send_tcp_command(f"client udpport {SWR_R_UDP_PORT}")
+
+
     try:
-        # Before sending the ATU command, drain any pending data
-        try:
-            flex6xxx_atu.tcp_socket.settimeout(0.1)  # Short timeout
-            while True:
-                flex6xxx_atu.tcp_socket.recv(BUFFER_SIZE)  # Drain buffer
-        except socket.timeout:
-            pass  # Buffer is now empty
-        finally:
-            flex6xxx_atu.tcp_socket.settimeout(10)  # Restore normal timeout
+        flush_tcp_buffer()
         print("atu: Sending tuning command...")
-
-        flex6xxx_atu.tcp_socket.send(f"C{flex6xxx_atu.cmd_counter}|atu start\n".encode("cp1252"))
-
-        time.sleep(1)
-        data_tcp = flex6xxx_atu.tcp_socket.recv(BUFFER_SIZE)
-        print(data_tcp)
-
-        expected_response = f"R{flex6xxx_atu.cmd_counter}|0|"
-
-        flex6xxx_atu.cmd_counter += 1
-        if (flex6xxx_atu.cmd_counter > MAX_CMD_NUM):
-            flex6xxx_atu.cmd_counter = BASE_CMD_NUM
-
-        if not data_tcp or (data_tcp.splitlines()[0] != expected_response.encode()):
-            raise ValueError("atu: Failed to tune the radio with the antenna!")
+        send_tcp_command(f"atu start")
 
         # Wait 15s for the wc of tunning time
         time.sleep(15)
         print("atu: Tuning successfully completed.")
+
+        swr = swr_read_value()
 
     except (socket.error, socket.timeout, BrokenPipeError) as e:
         print(f"atu: Connection error: {e}")
@@ -94,7 +145,7 @@ def flex6xxx_atu():
         flex6xxx_atu.tcp_socket = None
         raise RuntimeError(f"atu: Connection failed: {e}")
 
-    return 0
+    return swr
 
 def no_atu():
     return 0
